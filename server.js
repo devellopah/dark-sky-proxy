5759a8500b17f96fa1c1bd1c9403ce7744cfa65d
Islam Ibakaev <islamibakaev@gmail.com>
2017-06-05 23:49:14 +0300

    added nextReqAllowedFrom to response object

--- server.js
+++ server.js
@@ -1,37 +1,48 @@
-const Express = require('express')
 const path = require('path')
+
+const express = require('express')
 const favicon = require('serve-favicon')
-// const RateLimit = require('express-rate-limit');
 const cors = require('cors')
+
 const DarkSky = require('dark-sky')
+const RateLimit = require('express-rate-limit');
 
-const app = Express()
-app.use(favicon(path.join(process.cwd(), 'favicon.ico')))
-app.set('port', process.env.PORT || 5000)
+//darksky api key (you will get your own key by registering account at https://darksky.net/dev/)
+const key = require('./key')
 
-// const limiter = new RateLimit({
-//   windowMs: 10*60*1000, // 10 minutes
-//   max: 1, // limit each IP to 100 requests per windowMs
-//   delayMs: 0 // disable delaying - full speed until the max limit is reached
-// })
+const app = express()
 
-// app.use(limiter);
 app.use(cors())
+app.use(favicon(path.join(process.cwd(), 'favicon.ico')))
+
+app.set('port', process.env.PORT || 3000)
+
+app.enable('trust proxy')
+
+const sleep = 600000; // 10 minutes
+
+// the weather doesn't change too often
+const limiter = new RateLimit({
+  windowMs: sleep,
+  max: 1, // limit each IP to 1 requests per windowMs
+  delayMs: 0 // disable delaying - full speed until the max limit is reached
+})
 
 // Home
-app.get('/', function (req, res) {
-  res.send(`<h1 style="color: gray;">Hello from Weather Api!</h1><h2 style="color: cornflowerblue;">Current time is: ${ new Date().toLocaleString() }</h2>`)
+app.get('/', (req, res) => {
+  res.send(`<div>Current time is: ${ new Date().toLocaleString() }</div>`)
 })
 
 // DarkSky API
-const forecast = new DarkSky('ddd485ba03d64bb446db41178f9f6712')
+const forecast = new DarkSky(key)
 
-app.get('/api/v1/json', function (req, res) {
-  let lat = req.param('lat')
-  let lon = req.param('lon')
-  let units = req.param('units')
+app.get('/api/v1/json', limiter, (req, res) => {
+  const nextReqAllowedFrom = new Date(new Date().getTime() + sleep).toLocaleString()
 
-  res.setHeader("Content-Type", "application/json");
+  // console.log('req is reached', new Date().getTime())
+  console.log('rate limit', req.rateLimit)
+  console.log('next request allowed from', nextReqAllowedFrom)
+  const { lat, lon, units } = req.query
 
   forecast
     .latitude(lat)
@@ -40,17 +51,11 @@ app.get('/api/v1/json', function (req, res) {
     .language('en')
     .exclude('minutely,hourly,daily,alerts,flags')
     .get()
-    .then(response => res.json(response))
+    .then(weather => res.status(200).json(Object.assign(weather, { nextReqAllowedFrom })))
     .catch(error => res.send(error))
-
-  console.log(req.method + ': /api/v1/json' + lat + '/' + lon + '/' + units)
 })
 
-app.get('/headers', function(req,res){
-  res.set('Content-Type','text/plain');
-  var s = '';
-  for(var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
-    res.send(s);
-});
-
-app.listen(app.get('port'), () => console.log(`Server is listening at port ${app.get('port')}`))
+app.listen(
+  app.get('port'),
+  () => console.log(`Server is listening at port ${app.get('port')}`)
+)
